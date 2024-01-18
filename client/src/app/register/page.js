faea4d453feab4c23ad6d258c269c29b76b0c597
Sandip <c.restsandip123@gmail.com>
2024-01-18 08:07:20 +0545

    solve issue regarding form validation & userouter

--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -1,11 +1,12 @@
 "use client";
 import React, { useState } from "react";
-import { Formik, Form, useFormik } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 import Link from "next/link";
 import { Input } from "@nextui-org/react";
 import { toast } from "react-hot-toast";
 import { FaEye, FaEyeSlash } from "react-icons/fa6";
+import { useRouter } from "next/navigation";
 
 const SignupSchema = Yup.object().shape({
   fullName: Yup.string()
@@ -22,6 +23,7 @@ const SignupSchema = Yup.object().shape({
 });
 
 const Register = () => {
+  const router = useRouter();
   const [showSignupPassword, setShowSignupPassword] = useState(false);
   const [showReSignupPassword, setShowReSignupPassword] = useState(false);
 
@@ -35,6 +37,7 @@ const Register = () => {
       const result = await response.json();
       if (response.status === 201) {
         toast.success(result.msg);
+        router.push("/login");
       } else {
         toast.error(result.msg);
       }
@@ -51,7 +54,7 @@ const Register = () => {
         password: "",
         rePassword: "",
       },
-      SignupSchema,
+      validationSchema: SignupSchema,
       onSubmit: (values) => {
         handleRegister(values);
         resetForm();
@@ -61,124 +64,125 @@ const Register = () => {
   return (
     <div className="w-full h-screen flex flex-col gap-8 justify-center items-center">
       <h1 className="text-2xl font-semibold">Signup</h1>
-      <Formik>
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col items-center gap-6 w-1/3"
-        >
-          <div className="flex w-full gap-4">
-            <div className="flex flex-col items-start w-full">
-              <Input
-                type="text"
-                name="fullName"
-                variant="underlined"
-                label="Full Name"
-                value={values.fullName}
-                onChange={handleChange}
-              />
-              <div className="w-full h-2">
-                {errors.fullName && touched.fullName ? (
-                  <p className="text-sm text-brandColor font-medium">
-                    {errors.fullName}
-                  </p>
-                ) : null}
-              </div>
-            </div>
-            <div className="flex flex-col items-start w-full gap-1">
-              <Input
-                type="email"
-                name="email"
-                variant="underlined"
-                label="Email"
-                value={values.email}
-                onChange={handleChange}
-              />
-              <div className="w-full h-2">
-                {errors.email && touched.email ? (
-                  <p className="text-sm text-brandColor font-medium">
-                    {errors.email}
-                  </p>
-                ) : null}
-              </div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center gap-6 w-1/3"
+      >
+        <div className="flex w-full gap-4">
+          <div className="flex flex-col items-start w-full">
+            <Input
+              type="text"
+              name="fullName"
+              variant="underlined"
+              label="Full Name"
+              value={values.fullName}
+              onChange={handleChange}
+            />
+            <div className="w-full h-2">
+              {errors?.fullName && touched?.fullName ? (
+                <p className="text-sm text-brandColor font-medium">
+                  {errors.fullName}
+                </p>
+              ) : null}
             </div>
           </div>
-          <div className="flex w-full gap-4">
-            <div className="flex flex-col items-start w-full relative">
-              <Input
-                type={`${showSignupPassword ? "text" : "password"}`}
-                name="password"
-                variant="underlined"
-                label="Enter Password"
-                value={values.password}
-                onChange={handleChange}
-              />
-              <div className="w-full h-2">
-                {errors.password && touched.password ? (
-                  <p className="text-sm text-brandColor font-medium">
-                    {errors.password}
-                  </p>
-                ) : null}
-              </div>
-              {values.password.length > 0 && (
-                <span
-                  onClick={() => setShowSignupPassword(!showSignupPassword)}
-                  className="flex justify-center items-center cursor-pointer w-[20px] h-[20px] rounded-full bg-gray-200 text-xs absolute top-1/2 -translate-y-1/2 right-2"
-                >
-                  <FaEye
-                    className={`${
-                      showSignupPassword ? "inline-block" : "hidden"
-                    }`}
-                  />
-                  <FaEyeSlash
-                    className={`${
-                      showSignupPassword ? "hidden" : "inline-block"
-                    }`}
-                  />
-                </span>
-              )}
-            </div>
-            <div className="flex flex-col items-start w-full relative">
-              <Input
-                type={`${showReSignupPassword ? "text" : "password"}`}
-                name="rePassword"
-                variant="underlined"
-                label="Re-Enter Password"
-                value={values.rePassword}
-                onChange={handleChange}
-              />
-              <div className="w-full h-2">
-                {errors.rePassword && touched.rePassword ? (
-                  <p className="text-sm text-brandColor font-medium">
-                    {errors.rePassword}
-                  </p>
-                ) : null}
-              </div>
-              {values.rePassword.length > 0 && (
-                <span
-                  onClick={() => setShowReSignupPassword(!showReSignupPassword)}
-                  className="flex justify-center items-center cursor-pointer w-[20px] h-[20px] rounded-full bg-gray-200 text-xs absolute top-1/2 -translate-y-1/2 right-2"
-                >
-                  <FaEye
-                    className={`${
-                      showReSignupPassword ? "inline-block" : "hidden"
-                    }`}
-                  />
-                  <FaEyeSlash
-                    className={`${
-                      showReSignupPassword ? "hidden" : "inline-block"
-                    }`}
-                  />
-                </span>
-              )}
+          <div className="flex flex-col items-start w-full gap-1">
+            <Input
+              type="email"
+              name="email"
+              variant="underlined"
+              label="Email"
+              value={values.email}
+              onChange={handleChange}
+            />
+            <div className="w-full h-2">
+              {errors?.email && touched?.email ? (
+                <p className="text-sm text-brandColor font-medium">
+                  {errors.email}
+                </p>
+              ) : null}
             </div>
           </div>
-          <button type="submit" className="bg-thirdColor text-white py-1 px-2">
-            Sign Up
-          </button>
-        </form>
-      </Formik>
+        </div>
+        <div className="flex w-full gap-4">
+          <div className="flex flex-col items-start w-full relative">
+            <Input
+              type={`${showSignupPassword ? "text" : "password"}`}
+              name="password"
+              variant="underlined"
+              label="Enter Password"
+              value={values.password}
+              onChange={handleChange}
+            />
+            <div className="w-full h-2">
+              {errors?.password && touched?.password ? (
+                <p className="text-sm text-brandColor font-medium">
+                  {errors.password}
+                </p>
+              ) : null}
+            </div>
+            {values.password.length > 0 && (
+              <span
+                onClick={() => setShowSignupPassword(!showSignupPassword)}
+                className="flex justify-center items-center cursor-pointer w-[20px] h-[20px] rounded-full bg-gray-200 text-xs absolute top-1/2 -translate-y-1/2 right-2"
+              >
+                <FaEye
+                  className={`${
+                    showSignupPassword ? "inline-block" : "hidden"
+                  }`}
+                />
+                <FaEyeSlash
+                  className={`${
+                    showSignupPassword ? "hidden" : "inline-block"
+                  }`}
+                />
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col items-start w-full relative">
+            <Input
+              type={`${showReSignupPassword ? "text" : "password"}`}
+              name="rePassword"
+              variant="underlined"
+              label="Re-Enter Password"
+              value={values.rePassword}
+              onChange={handleChange}
+            />
+            <div className="w-full h-2">
+              {errors?.rePassword && touched?.rePassword ? (
+                <p className="text-sm text-brandColor font-medium">
+                  {errors.rePassword}
+                </p>
+              ) : null}
+            </div>
+            {values.rePassword.length > 0 && (
+              <span
+                onClick={() => setShowReSignupPassword(!showReSignupPassword)}
+                className="flex justify-center items-center cursor-pointer w-[20px] h-[20px] rounded-full bg-gray-200 text-xs absolute top-1/2 -translate-y-1/2 right-2"
+              >
+                <FaEye
+                  className={`${
+                    showReSignupPassword ? "inline-block" : "hidden"
+                  }`}
+                />
+                <FaEyeSlash
+                  className={`${
+                    showReSignupPassword ? "hidden" : "inline-block"
+                  }`}
+                />
+              </span>
+            )}
+          </div>
+        </div>
+        <button type="submit" className="bg-thirdColor text-white py-1 px-2">
+          Sign Up
+        </button>
+      </form>
       <p>
-        Already have an account? <Link href="/login" className="text-red-600 font-medium underline">Login</Link>
+        Already have an account?{" "}
+        <Link href="/login" className="text-red-600 font-medium underline">
+          Login
+        </Link>
       </p>
     </div>
   );
