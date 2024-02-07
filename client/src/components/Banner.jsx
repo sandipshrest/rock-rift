0eefe120e63948c4955d1f890db2bf5162c57d6c
Sandip <c.restsandip123@gmail.com>
2024-02-07 23:30:42 +0545

    edit product & add slogan in banner section

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -15,10 +15,11 @@ import "swiper/css/scrollbar";
 import "swiper/css/effect-fade";
 import BannerData from "@/data/BannerData";
 import Image from "next/image";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Banner = () => {
   return (
-    <section className="bg-gray-200">
+    <section className="bg-red-100">
       <Swiper
         modules={[Pagination, Scrollbar, A11y, Autoplay, EffectFade]}
         spaceBetween={10}
@@ -46,13 +47,16 @@ const Banner = () => {
           <SwiperSlide key={index}>
             <div className="container w-full flex pt-16">
               <div className="w-2/3 flex items-center inset-0 pt-[78px]">
-                <div className="xl:w-1/2 md:w-2/3 w-full flex flex-col items-start gap-3">
-                  <h1 className="lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-semibold text-black">
+                <div className="lg:w-4/5 md:w-2/3 w-full flex flex-col items-start gap-4">
+                  <h1 className="lg:text-7xl md:text-5xl sm:text-4xl text-3xl font-extrabold text-black">
                     <span className="mr-1 text-brand1">
                       {item.slogan.split(" ")[0]}
                     </span>
                     {item.slogan.split(" ").slice(1).join(" ")}
                   </h1>
+                  <button className="py-2 px-3 bg-red-500 border border-red-500 text-white text-lg font-medium flex items-center gap-2 rounded-sm">
+                    Buy Now <FaArrowRightLong />
+                  </button>
                 </div>
               </div>
               <div className="w-1/3">
@@ -60,7 +64,7 @@ const Banner = () => {
                   src={item.image}
                   priority={true}
                   height={6000}
-                  width={6000} 
+                  width={6000}
                   className="w-full lg:h-[800px] md:h-[600px] sm:h-[500px] h-[350px] object-cover object-top"
                   alt="banner-image"
                 />
--- client/src/data/BannerData.js
+++ client/src/data/BannerData.js
@@ -1,13 +1,18 @@
 const data = [
-    {
-        id:1,
-        image: '/images/banner-image/image1.png',
-        slogan: ''
-    },
-    {
-        id:2,
-        image: '/images/banner-image/image2.png',
-        slogan: ''
-    }
-]
+  {
+    id: 1,
+    image: "/images/banner-image/image1.png",
+    slogan: "Rock Your World with Band-Inspired Fashion",
+  },
+  {
+    id: 2,
+    image: "/images/banner-image/image2.png",
+    slogan: "Wear Your Passion, Live Your Style",
+  },
+  {
+    id: 2,
+    image: "/images/banner-image/image3.png",
+    slogan: "Elevate Your Wardrobe with Anime Vibes",
+  },
+];
 export default data;
