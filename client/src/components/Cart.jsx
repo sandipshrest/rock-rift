58ea12684f0076e03b2aeeb8caadcb43be5045f4
Sandip <c.restsandip123@gmail.com>
2024-03-11 13:15:40 +0545

    trying to use nextui table in cart

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,25 +1,114 @@
 "use client";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  User,
+  Chip,
+  Tooltip,
+  getKeyValue,
+} from "@nextui-org/react";
+import { columns } from "../data/CartData";
 
-const Cart = ({ cartItems, toggleCart }) => {
-  // const [cartItems, setCartItems] = useState([]);
-  // const fetchCarts = async () => {
-  //   try {
-  //     const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/carts`);
-  //     setCartItems(data);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // useEffect(() => {
-  //   fetchCarts();
-  // }, []);
+const UserTable = (props) => {
+  const renderCell = useCallback((user, columnKey) => {
+    const cellValue = user[columnKey];
+
+    switch (columnKey) {
+      case "name":
+        return (
+          <User
+            avatarProps={{ radius: "lg", src: user.avatar }}
+            description={user.email}
+            name={cellValue}
+          >
+            {user.email}
+          </User>
+        );
+      case "role":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-sm capitalize">{cellValue}</p>
+            <p className="text-bold text-sm capitalize text-default-400">
+              {user.team}
+            </p>
+          </div>
+        );
+      case "status":
+        return (
+          <Chip
+            className="capitalize"
+            color={statusColorMap[user.status]}
+            size="sm"
+            variant="flat"
+          >
+            {cellValue}
+          </Chip>
+        );
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <Tooltip content="Details">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                view
+              </span>
+            </Tooltip>
+            <Tooltip content="Edit user">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                edit
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                delete
+              </span>
+            </Tooltip>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
 
   return (
+    <Table aria-label="Example table with custom cells">
+      <TableHeader columns={columns}>
+        {(column) => (
+          <TableColumn
+            key={column.uid}
+            align={column.uid === "actions" ? "center" : "start"}
+          >
+            {column.name}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody items={props.cartList}>
+        {(item) => (
+          <TableRow key={item}>
+            {(columnKey) => {
+              return (
+                <TableCell>
+                  <div>{renderCell(item, columnKey)}</div>
+                </TableCell>
+              );
+            }}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+};
+
+const Cart = ({ cartItems, toggleCart }) => {
+  return (
     <section
-      className={`fixed w-96 h-screen py-14 border border-black bg-white top-0 transition-all duration-500 ease-linear ${
-        toggleCart ? "left-0" : "-left-96"
+      className={`fixed w-[800px] h-screen py-14 border border-black bg-white top-0 transition-all duration-[1s] ease-linear ${
+        toggleCart ? "left-0" : "-left-[800px]"
       }`}
     >
       <div className="px-10 pb-3 border-b border-black">
@@ -27,13 +116,7 @@ const Cart = ({ cartItems, toggleCart }) => {
       </div>
       <div className="px-10 pt-4">
         {cartItems.length > 0 ? (
-          <div>
-            {cartItems?.map((item, id) => (
-              <div key={id}>
-                <h3>{item.product}</h3>
-              </div>
-            ))}
-          </div>
+          <UserTable cartList={cartItems} />
         ) : (
           <div>
             <h3>Your Cart is empty</h3>
---
+++ client/src/data/CartData.js
@@ -0,0 +1,7 @@
+const columns = [
+  { name: "Email", uid: "name" },
+  { name: "Role", uid: "role" },
+  { name: "Full Name", uid: "fullName" },
+];
+
+export { columns };
